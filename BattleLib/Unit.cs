799b8b97090b367ff23ef0611da5f7ef035d101d
baumg <baumg@DESKTOP-712A7RT>
2016-05-29 08:44:57 -0400

    Add project files.

---
+++ BattleLib/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleLib
+{
+    public class Unit
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public bool Alive { get; set; }
+
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClient.MagisterService;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MagisterBattleClient client = new MagisterBattleClient())
+            {
+                Console.WriteLine("User Name:  ");
+                string userName = Console.ReadLine();
+
+                Console.WriteLine("Password:  ");
+                string password = Console.ReadLine();
+
+                string key = client.LogIn(userName, password);
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
